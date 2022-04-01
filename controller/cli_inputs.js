52201c22989afd0dc2f933a5a063fb5a082be7cc
felipesantos94 <felipe.fars94@gmail.com>
2022-04-01 19:39:03 +0200

    creating basic user input flow and creation of config file

--- controller/cli_inputs.js
+++ controller/cli_inputs.js
@@ -3,64 +3,89 @@ import boxen from "boxen";
 import figlet from "figlet";
 import gradient from "gradient-string";
 import chalkAnimation from "chalk-animation";
+import {
+  createSpinner
+} from 'nanospinner';
 import readline from "readline";
 import fs from "fs";
+import inquirer from "inquirer";
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-const sleep = (ms = 1000) => new Promise((r) => setTimeout(r, ms));
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
 
 async function start() {
-  const rainbowTitle = chalkAnimation.rainbow("CLI Masters \n");
-  await sleep();
-  rainbowTitle.stop();
+  console.clear();
 
-  figlet(`Crypto Data Export`, {
-    font: "3-D"
+  figlet('Crypto Export API', {
+    font: 'The Edge'
   }, (err, data) => {
-    console.log(gradient.pastel.multiline(data) + "\n");
-
+    if (err) throw new Error(err);
+    console.log(gradient.pastel.multiline(data) + '\n');
     console.log(
-      boxen("Crypto Currency Historical Data Export CLI", {
-        borderColor: "blackBright",
-        borderStyle: "bold",
+      boxen('Crypto Currency Historical Data Export CLI', {
+        borderColor: 'blackBright',
+        borderStyle: 'bold',
       })
     );
-
     console.log(`
-        ${chalk.green("Welcome!")} 
-        Lorem ipsum dolor sit amet. 
-        Id assumenda rerum est tempore nulla est sunt temporibus ut quia veniam id ipsam fugit quo velit corporis qui voluptatem sint. 
-        Qui galisum dolores ut molestias sint et temporibus necessitatibus!
-      `);
-  });
+        ${chalk.green('Welcome!')} 
+      This small application is made by the swaggest swaggers in the whole World Wide Web 2.0 (Soon to be 3.0, hide your daughters).
 
-  // Check if user is registered, if not, register user
-  if (fs.existsSync("./controller/config.json")) {
-    await sleep(2000);
-    console.log("Already registered");
-  } else {
-    await questionAPI();
-  }
+      You can do lots of stuff like exporting crypto data into excel and many more (not really).
+      Let's start! :)
+      `);
+
+      initialSetup();
+  });
 }
 
-// Asks API key & writes it to config.json
-async function questionAPI() {
-  await doubleSleep();
-  rl.question("Please enter your CryptoWatch API key: ", function (apiKey) {
-    const client = {
-      apiKey: apiKey,
-    };
+// Initial user application setup
+async function initialSetup() {
+  // Check if user has saved API and/or DB information.
+  const configExists = fs.existsSync('./config.json');
+  const spinner = createSpinner('Checking config.json...').start();
+  await sleep();
 
-    const data = JSON.stringify(client);
-    fs.writeFile("./controller/config.json", data, (err) => {
-      if (err) throw err;
-      console.log("API key saved");
+  if (!configExists) {
+    spinner.error({
+      text: 'No "config.json" file found.'
     });
-  });
+
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    const apiKeyInputHandler = function () {
+      rl.question('Please enter your CryptoWatch API key: ', function (apiKey) {
+        if (typeof apiKey !== 'string') {
+          console.error('Invalid API Key.')
+          console.info('Please try again.');
+          apiKeyInputHandler();
+        }
+
+        // Creating new config.json file in root folder.
+        const config = {
+          apiKeys: {
+            cryptoWatch: apiKey
+          }
+        };
+
+        fs.writeFile('./config.json', JSON.stringify(config, null, 2), (err) => {
+          if (err) throw new Error('Something went wrong! Error: ', err);
+          console.log('API key saved');
+
+          process.exit(1) // TODO next step - The app should not exit after api key saved
+        });
+      });
+    }
+
+    apiKeyInputHandler();
+
+  } else if (configExists) {
+    // TODO next step - The app should prompt the user with the next steps
+    spinner.success({ text: 'That\'s all. Thanks a lot for using the app. :D',});
+    process.exit(1); // not final code
+  }
 }
 
 export {
