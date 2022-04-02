6b7668ae034c1bc9069734d56c7cf24c80de3fbd
felipesantos94 <felipe.fars94@gmail.com>
2022-04-02 16:59:43 +0200

    lint fix

--- src/controllers/inputs.js
+++ src/controllers/inputs.js
@@ -1,42 +1,15 @@
-import chalk from "chalk";
-import boxen from "boxen";
-import figlet from "figlet";
-import gradient from "gradient-string";
-import chalkAnimation from "chalk-animation";
-import {
-  createSpinner
-} from 'nanospinner';
-import readline from "readline";
-import fs from "fs";
-import inquirer from "inquirer";
+/* eslint-disable no-promise-executor-return */
+import chalk from 'chalk';
+import boxen from 'boxen';
+import figlet from 'figlet';
+import gradient from 'gradient-string';
+import chalkAnimation from 'chalk-animation';
+import { createSpinner } from 'nanospinner';
+import readline from 'readline';
+import fs from 'fs';
+import inquirer from 'inquirer';
 
 const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
-
-async function start() {
-  console.clear();
-
-  figlet('Crypto Export API', {
-    font: 'The Edge'
-  }, (err, data) => {
-    if (err) throw new Error(err);
-    console.log(gradient.pastel.multiline(data) + '\n');
-    console.log(
-      boxen('Crypto Currency Historical Data Export CLI', {
-        borderColor: 'blackBright',
-        borderStyle: 'bold',
-      })
-    );
-    console.log(`
-        ${chalk.green('Welcome!')} 
-      This small application is made by the swaggest swaggers in the whole World Wide Web 2.0 (Soon to be 3.0, hide your daughters).
-
-      You can do lots of stuff like exporting crypto data into excel and many more (not really).
-      Let's start! :)
-      `);
-
-      initialSetup();
-  });
-}
 
 // Initial user application setup
 async function initialSetup() {
@@ -47,7 +20,7 @@ async function initialSetup() {
 
   if (!configExists) {
     spinner.error({
-      text: 'No "config.json" file found.'
+      text: 'No "config.json" file found.',
     });
 
     const rl = readline.createInterface({
@@ -55,10 +28,10 @@ async function initialSetup() {
       output: process.stdout,
     });
 
-    const apiKeyInputHandler = function () {
-      rl.question('Please enter your CryptoWatch API key: ', function (apiKey) {
+    const apiKeyInputHandler = () => {
+      rl.question('Please enter your CryptoWatch API key: ', (apiKey) => {
         if (typeof apiKey !== 'string') {
-          console.error('Invalid API Key.')
+          console.error('Invalid API Key.');
           console.info('Please try again.');
           apiKeyInputHandler();
         }
@@ -66,27 +39,49 @@ async function initialSetup() {
         // Creating new config.json file in root folder.
         const config = {
           apiKeys: {
-            cryptoWatch: apiKey
-          }
+            cryptoWatch: apiKey,
+          },
         };
 
         fs.writeFile('./config.json', JSON.stringify(config, null, 2), (err) => {
           if (err) throw new Error('Something went wrong! Error: ', err);
           console.log('API key saved');
-          
         });
       });
-    }
+    };
 
     apiKeyInputHandler();
-
   } else if (configExists) {
     // TODO next step - The app should prompt the user with the next steps
-    spinner.success({ text: 'That\'s all. Thanks a lot for using the app. :D',});
+    spinner.success({ text: 'That\'s all. Thanks a lot for using the app. :D' });
     process.exit(1); // not final code
   }
 }
 
-export {
-  start
-};
+async function start() {
+  console.clear();
+
+  figlet('Crypto Export API', {
+    font: 'The Edge',
+  }, (err, data) => {
+    if (err) throw new Error(err);
+    console.log(`${gradient.pastel.multiline(data)}\n`);
+    console.log(
+      boxen('Crypto Currency Historical Data Export CLI', {
+        borderColor: 'blackBright',
+        borderStyle: 'bold',
+      }),
+    );
+    console.log(`
+        ${chalk.green('Welcome!')} 
+      This small application is made by the swaggest swaggers in the whole World Wide Web 2.0 (Soon to be 3.0, hide your daughters).
+
+      You can do lots of stuff like exporting crypto data into excel and many more (not really).
+      Let's start! :)
+      `);
+
+    initialSetup();
+  });
+}
+
+export default start;
