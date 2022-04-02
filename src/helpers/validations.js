74a44fc2737526712d458ee67d622e83a9d9fde7
felipesantos94 <felipe.fars94@gmail.com>
2022-04-02 14:54:29 +0200

    adding helpers description and minor clean

--- src/helpers/validations.js
+++ src/helpers/validations.js
@@ -1,7 +1,16 @@
-export const isNotEmptyArray = (el) => (Array.isArray(el) && el.length > 0);
+/**
+ * @description       Value validations that properly check truthy/falsy values for a better control flow.
+ * 
+ * @isNotEmptyArray   To check if array is empty, since [] is evaluated as truthy value.
+ * @isNotEmptyObject  Same principle as above but with objects. 
+ * @hasValue          Check if the variable has any possible valid value, the falsy "0" (zero) included.
+ * @isString          Check if it is really a string!!
+ */
+
+export const isNotEmptyArray = (arr) => (Array.isArray(arr) && arr.length > 0);
 
 export const isNotEmptyObject = (obj) => !(obj && obj.constructor === Object && Object.entries(obj).length === 0);
 
-export const hasAnyValue = (el) => (el || typeof el === 'number');
+export const hasValue = (el) => (el || typeof el === 'number');
 
-export const isString = (string) => (string && typeof string === "string");
+export const isString = (str) => (str && typeof str === "string");
