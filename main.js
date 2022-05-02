21a6d21f73bbc61673c8b96b9d6561613f0cd08d
felipesantos94 <felipe.fars94@gmail.com>
2022-05-03 00:28:32 +0200

    main file cleanup

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-import { start, setParams } from "./src/controllers/inputs.js";
-import { exportXLSX, exportCSV } from "./src/controllers/export.js";
+import {welcome} from "./src/controllers/inputs.js";
 
-await start();
-const data = await setParams();
-console.log(""); // Jump a line
-if (data.format === "xlsx") {
-  await exportXLSX(data.cw[data.params.periods]);
-} else {
-  await exportCSV(data.cw[data.params.periods]);
-}
+await welcome();
