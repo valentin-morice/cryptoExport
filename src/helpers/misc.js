daf7ff8be47971ee37b5c6078dd21330f6037ef1
felipesantos94 <felipe.fars94@gmail.com>
2022-05-07 14:21:34 +0200

    apiError improv to handle rest api errors

--- src/helpers/misc.js
+++ src/helpers/misc.js
@@ -1,3 +1,5 @@
+import chalk from "chalk";
+
 /**
  * @description General helpers
  * 
@@ -10,9 +12,19 @@ export function sleep(ms = 2000) {
   return new Promise((r) => setTimeout(r, ms));
 }
 
-export async function apiError(msg, err) {
+export function apiError(msg, err) {
+  let restError = "";
+  if (err.response.error) {
+    const errJson = JSON.parse(err.response.error.text);
+    
+    if (errJson.allowance && errJson.allowance.upgrade) {
+      errJson.error = `Market not available. ${errJson.allowance.upgrade}.`;
+    }
+
+    restError = `${errJson.error} Status code: ${err.response.error.status}`;
+  }
+  
   throw new Error(
-    `${msg}\n${chalk.blackBright("If the problem persists, please contact us at: https://github.com/aange-marcel/cryptoExport/issues/new.\n")}`,
-    {cause: err}
+    `${chalk.bgYellow.red(restError ? restError : msg)}\n${chalk.red("If the problem persists, please contact us at: https://github.com/aange-marcel/cryptoExport/issues/new.\n")}`, (err && !err.response.error) ? {cause: err} : ""
   );
 }
